c7e569df630b59b2d41223ab7aa32d08baa0d7df
Temur9 <99079769+Temur9@users.noreply.github.com>
2022-12-25 11:11:44 +0500

    added link

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -67,7 +67,7 @@ const About = () => {
                 </p>
               </div>
               <div className="resume-download">
-                <a href="/#">
+                <a href="https://drive.google.com/file/d/1TBQReVaf_HLf3YHfVFh1SUoSwAsRYDRS/view?usp=sharing">
                   Download my resume
                 </a>
               </div>
